feb09cb7741760e17bb1c8bde84c7e3f6d5eabec
Kimani Walker <kimaniwalker@gmail.com>
2019-01-19 11:20:22 -0600

    Hit Ghibli API , Pulls back data objects in console

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const Films_Url = "https://ghibliapi.herokuapp.com/films";
+const People_Url = "https://ghibliapi.herokuapp.com/people"
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      list: [],
+      type:""
+    }; 
+  }
+
+  showMovies = async() => {
+    try {
+      let res = await fetch(`${Films_Url}`);
+      let data = await res.json();
+      console.log(data);
+
+      this.setState({
+        list: data,
+        type: "movies"
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  showPeople = async() => {
+    try {
+      let res = await fetch(`${People_Url}`);
+      let data = await res.json();
+      console.log(data);
+
+      this.setState({
+        list: data,
+        type: "people"
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        
+        <div>
+          
+          <h1>Studio Ghibli Api </h1>
+
+        </div>
+
+        <div className="d-flex justify-content-left">
+          <button
+            className="btn btn-dark btn-md m-2"
+            onClick={this.showMovies}
+          >Movies</button>
+          </div>
+
+          <div className="d-flex justify-content-left">
+          <button
+            className="btn btn-dark btn-md m-2"
+            onClick={this.showPeople}
+          >People</button>
+          </div>
+
+      </React.Fragment>
+      
+      
+      
     );
   }
 }
